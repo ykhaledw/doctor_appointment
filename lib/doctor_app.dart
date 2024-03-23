92fbbfa02b8e72e7ff07344753b64ff66ddb73d1
youssefwahdan12 <youssefwahdan12@gmail.com>
2024-03-23 15:36:06 +0000

    updating DoctorApp screen

--- lib/doctor_app.dart
+++ lib/doctor_app.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:myapp/core/routing/app_router.dart';
+import 'package:myapp/core/routing/routes.dart';
 
 class DoctorApp extends StatelessWidget {
   final AppRouter appRouter;
@@ -7,6 +10,15 @@ class DoctorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      designSize: const Size(375, 812),
+      child: MaterialApp(
+        title: 'Doctor Appointment',
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
   }
 }
