d5b60762cfcd619c66f790400393bff650721eaa
youssefwahdan12 <youssefwahdan12@gmail.com>
2024-04-02 12:09:42 +0000

    create dependency injection file

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:myapp/core/networking/api_service.dart';
+import 'package:myapp/core/networking/dio_factory.dart';
+import 'package:myapp/features/login_screen/data/repos/login_repo.dart';
+import 'package:myapp/features/login_screen/logic/cubit/login_cubit.dart';
+
+
+final getIt = GetIt.instance;
+
+Future<void> setupGetIt() async{
+
+  //Dio & ApiService
+  Dio dio = DioFactory.getDio();
+  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+
+  //Login
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(apiService: getIt()));
+  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+}
--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:myapp/core/di/dependency_injection.dart';
 import 'package:myapp/core/routing/routes.dart';
+import 'package:myapp/features/login_screen/logic/cubit/login_cubit.dart';
 import 'package:myapp/features/login_screen/ui/login_screen.dart';
 import 'package:myapp/features/onboarding_screen/ui/onboarding_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
-    switch (settings.name){
+    switch (settings.name) {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(
-          builder: (_)=> const OnBoardingScreen(),
-          );
+          builder: (_) => const OnBoardingScreen(),
+        );
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_)=> const LoginScreen(),
-          );
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
+        );
       default:
-      return MaterialPageRoute(
+        return MaterialPageRoute(
           builder: (_) => Scaffold(
             body: Center(
               child: Text('No route defined for ${settings.name}'),
