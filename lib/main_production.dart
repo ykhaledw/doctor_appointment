e35ad1d4fd3abaeaac5adcd3b5c3b4280e045978
youssefwahdan12 <youssefwahdan12@gmail.com>
2024-04-21 13:25:58 +0000

    fix texts being hidden bug in release mode.

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:myapp/core/di/dependency_injection.dart';
 import 'package:myapp/core/routing/app_router.dart';
 import 'package:myapp/doctor_app.dart';
 
-void main() {
+void main() async{
   setupGetIt();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
   runApp(DoctorApp(
     appRouter: AppRouter(),
   ));
